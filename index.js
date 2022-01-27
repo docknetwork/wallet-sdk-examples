43c118577f8a1a84cdcddceb9d00c2ed30b47ad9
Maycon Mello <maycon.mellos@gmail.com>
2022-01-27 16:00:52 -0300

    nodejs updates

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
+import { NetworkManager } from "@docknetwork/wallet-sdk-core/lib/modules/network-manager";
 import { Wallet } from "@docknetwork/wallet-sdk-core/lib/modules/Wallet";
-import { waitFor } from "@docknetwork/wallet-sdk-core/lib/test-utils";
+import { Transactions } from "@docknetwork/wallet-sdk-transactions/lib/transactions";
+
+NetworkManager.getInstance().setNetworkId("testnet");
 
 async function main() {
   const wallet = await Wallet.create();
 
   console.log("Wallet crated");
 
-  const account = await wallet.accounts.create({ name: "test2" });
+  const account1 = await wallet.accounts.getOrCreate({
+    name: "Account 1",
+    mnemonic:
+      "opera creek math job debate rib royal immense report sick used earth",
+  });
 
-  console.log("Account: ", account.address);
+  const account2 = await wallet.accounts.getOrCreate({
+    name: "Account 2",
+    mnemonic:
+      "autumn sentence spot genuine tribe frequent photo draft leg proof aunt knock",
+  });
+
+  console.log("Account1 address: ", account1.address);
+  console.log("Account1 balance: ", await account1.getBalance());
+
+  const accountTx = Transactions.with(account1);
+
+  const fee = await accountTx.getFee({
+    toAddress: account2.address,
+    amount: 1,
+  });
+
+  console.log(`Fee ${fee}`);
 }
 
 main();
