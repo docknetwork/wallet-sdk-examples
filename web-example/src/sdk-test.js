dc2372dff0476c507f9e519c3cc5f12823fa444a
Maycon Mello <maycon.mellos@gmail.com>
2023-07-24 17:45:09 -0300

    refactoring

---
+++ web-example/src/sdk-test.js
@@ -0,0 +1,37 @@
+import { createWallet } from "@docknetwork/wallet-sdk-core/lib/wallet";
+import { createAccountProvider } from "@docknetwork/wallet-sdk-core/lib/account-provider";
+
+
+async function main() {
+    const wallet = await createWallet({
+        databasePath: "data-store",
+        dbType: "sqljs",
+        typeORMConfigs: {
+            location: "data-store",
+            sqlJsConfig: {
+                locateFile: file => `https://sql.js.org/dist/${file}`
+            },
+        }
+    });
+
+    window.wallet = wallet;
+
+    const accountsProvider = createAccountProvider({
+        wallet,
+    });
+
+    const account = await accountsProvider.create({
+        name: 'test',
+    });
+
+    console.log(account);
+
+    // manage DIDs
+
+    const documents = await wallet.getAllDocuments();
+
+    console.log(documents);
+}
+
+
+main();
