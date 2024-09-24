8c5fb32fb74c225867b6448cedde66b3c9be5673
Maycon <maycon.mellos@gmail.com>
2024-09-24 14:51:49 -0300

    removing commented code

--- web-example/src/index.js
+++ web-example/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import './sdk-test';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
